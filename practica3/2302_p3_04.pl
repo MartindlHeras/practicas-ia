7f6c1cb60faf537bccae41c93be0c30598e9a917
Santi <santiago.valderrabano@estudiante.uam.es>
2019-04-05 12:58:38 +0200

    Version 1.2 (Glet)
      - El 6 no va del todo bien.

--- practica3/2302_p3_04.pl
+++ practica3/2302_p3_04.pl
@@ -31,3 +31,54 @@ palindromo(L) :- invierte(L,L).
 
 divide(L, 0, [], L).
 divide([X | L], N, L1, L2) :- N1 is N - 1, divide(L, N1, P, L2), concatena([X], P, L1).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% EJERCICIO 5
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+aplasta([], []).
+aplasta([L|L1], LAplastado) :-
+    aplasta(L, NuevaL),
+    aplasta(L1, NuevaLs),
+    append(NuevaL, NuevaLs, LAplastado).
+aplasta(L, [L]).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% EJERCICIO 6
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%divisible(X,Y) :- 0 is X mod Y, !.
+
+%divisible(X,Y) :- X > Y+1, divisible(X, Y+1).
+%isPrime(2) :- true,!.
+%isPrime(X) :- X < 2,!,false.
+%isPrime(X) :- not(divisible(X, 2)).
+%
+divisible(X,Y):-
+N is Y*Y,
+N =< X,
+X mod Y =:= 0.
+
+divisible(X,Y):-
+Y < X,
+Y1 is Y+1,
+divisible(X,Y1).
+
+isPrime(X):-
+Y is 2, X > 1, \+divisible(X,Y).
+
+next_factor(_, 2, 3).
+next_factor(N, F, NF):-
+    F < ceil(sqrt(N)),
+    NF is F + 2.
+
+primos(1,[]).
+primos(X, [Factor1|L]):-
+    between(2, X, Factor1),
+    isPrime(Factor1),
+    (X mod Factor1) =:= 0,
+    NuevoX is X // Factor1,
+    next_factor(NuevoX, Factor1, NF),
+    primos(NuevoX, L).
